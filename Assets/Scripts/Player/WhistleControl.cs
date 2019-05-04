08a48acc2f453cc7222b4cfb816c216b8f65b369
AJ Abotomey <killerbyte_93@hotmail.com>
2019-05-04 15:47:08 +1000

    Necessary housekeeping + Addition of Whistling
    Cleaned up the project
    Upgraded to Unity 2019
    Added the ability for the player to whistle.

---
+++ Assets/Scripts/Player/WhistleControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhistleControl : MonoBehaviour
+{
+    [SerializeField] private AudioClip sound;
+    [SerializeField] private AudioSource source;
+
+    void Start()
+    {
+        source.clip = sound;
+        source.loop = false;
+    }
+
+    void Update()
+    {
+        bool whistle = Input.GetButtonDown("Whistle");
+
+        if (whistle && source.isPlaying)
+        {
+            Debug.Log("Should be whistling.");
+            Whistle();
+        }
+    }
+
+    public void Whistle()
+    {
+        source.Play();
+    }
+}
